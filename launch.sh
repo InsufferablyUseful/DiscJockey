468fe1e34c0c71c423644709c2b0a252ad13d67d
Ben Robertson <benrobertson150@hotmail.co.uk>
2026-08-08 00:49:14 +0100

    Add GPL V3 license

--- launch.sh
+++ launch.sh
@@ -1,5 +1,20 @@
-#!/bin/bash
+#DiscJockey - Automatically runs windows installers from removeable media 
+#    Copyright (C) 2026  InsufferablyUseful
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#!/bin/bash
 discDirectory="$1"
 cd "$discDirectory"
 #Grab the config file
